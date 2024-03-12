efbb36ecd2a3e95bf8ec0b1f8587428cd26ce84a
laboon2 <rodriguez.aza.john@gmail.com>
2024-03-12 14:46:04 -0500

    add list categorys in categorypage

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import CategoryForm from "../components/CategoryForm";
+import { useCategories } from "../context/CategoryContext";
 function CategoryPage() {
+  const {
+    getCategories,
+    categories,
+    listSubCategories,
+    setListSubCategories,
+    subcategories,
+    subcategoriesQuestion,
+  } = useCategories();
+
+  useEffect(() => {
+    getCategories()
+  }, [])
+  
   return (
     <div className="h-screen grid grid-flow-row md:grid-cols-3 gap-4 ">
       <div className="row-span-2 col-span-2 md:col-span-1  overflow-auto touch-pan-y">
-        <span>OBject Array</span>
+      {categories.map((category) => (
+            <p key={category.id_category}>{category.name_category}</p>
+          ))}
       </div>
       <div className=" row-span-2 col-span-2 ">
         <div className=" flex  items-center justify-center">
